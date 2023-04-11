a3586102d804c8ecd87af47f2d054834a1086a17
Anna78990 <takasuanna1990@gmail.com>
2023-04-11 15:58:20 +0000

    4

--- 0x01-graphs/4-depth_first_traverse.c
+++ 0x01-graphs/4-depth_first_traverse.c
@@ -1,76 +1,53 @@
 #include "graphs.h"
 
-void new_array_elem(vertex_t **array, vertex_t *dest)
-{
-	int i;
+/**
+ * depth_first_search - traverse graph
+ * @vertex: pointer vertex node
+ * @visited: array to search if it is visited or not
+ * @action: pointer to function
+ * @depth: current depth
+ * @max_depth: max depth
+ */
 
-	for (i = 0; array[i]; i++);
-	array[i] == dest
-	
-}
-
-int search_array(vertex_t **array, vertex_t *dest)
-{
-	int i = 0;
-
-	if (!dest)
-		return (0);
-	for (; array[i]; i++)
-	{
-		if (array[i] == dest)
-			return (0);
-	};
-	return (1);
-}
-
-size_t search_deadend(vertex_t **array, edge_t *edge, size_t size,
-		void (*action)(const vertex_t *v, size_t depth))
+void depth_first_search(vertex_t *vertex, int *visited,
+		void (*action)(const vertex_t *vertex, size_t depth), size_t depth,
+		size_t *max_depth)
 {
 	edge_t *e;
 
-	if (search_array(array, edge->dest) == 1)
-	{
-		new_array_elem(array, edge->dest);
-		action(edge->dest, size);
-		e = edge->dest->edges;
-		if (e)
-			return (search_deadend(array, e, size + 1, action));
-		return (search_deadend(array, edge->next, size, action));
-	}
-	if (search_array(array, edge->dest) == 0)
-	{
-		if (edge->next)
-			return (search_deadend(array, edge->next, size, action));
-		return (size);
-	}
+	if (!vertex || visited[vertex->index] == 1)
+		return;
+	visited[vertex->index] = 1;
+	action(vertex, depth);
+	if (depth > *max_depth)
+		*max_depth = depth;
+	depth++;
+	for (e = vertex->edges; e; e = e->next)
+		depth_first_search(e->dest, visited, action, depth, max_depth);
 }
 
 /**
- * graph_delete - free the graph memories
+ * depth_first_traverse - free the graph memories
  * @graph: pointer to graph
+ * @action: pointer to function to use
+ *          v: pointer to vertex
+ *          depth: depth of vertex
  *
- * Return: always nothing
+ * Return: maximum of depth, 0 on failure
  */
 size_t depth_first_traverse(const graph_t *graph,
 		void (*action)(const vertex_t *v, size_t depth))
 {
-	vertex_t **array_dest;
-	vertex_t *vertex = graph->vertices;
-	size_t depth = 0, i = 0, max_depth = 0;
+	int *visited;
+	size_t depth_max;
 
-	if (graph->vertices == NULL)
+	if (!graph || !action || !graph->nb_vertices)
 		return (0);
-	array_dest = (vertex_t **)malloc(sizeof(vertex_t) * graph->nb_vertices);
-	while (vertex)
-	{
-		if (search_array(array_dest, vertex) == 1)
-			action(vertex, depth);
-		if (vertex->edges)
-			i = search_deadend(array_dest, vertex->edges, depth + 1, action);
-		if (i > max_depth)
-			max_depth = i;
-		vertex = vertex->next;
-	}
-	free(array_dest);
-	return (max_depth);
+	visited = calloc(graph->nb_vertices, sizeof(*visited));
+	if (!visited)
+		return (0);
+	depth_max = 0;
+	depth_first_search(graph->vertices, visited, action, 0, &depth_max);
+	free(visited);
+	return (depth_max);
 }
