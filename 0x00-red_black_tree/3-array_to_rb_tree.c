68ac43f69f32464d6f9c9a031fd14ef247698b90
Anna78990 <takasuanna1990@gmail.com>
2023-06-09 04:34:17 +0000

    3

---
+++ 0x00-red_black_tree/3-array_to_rb_tree.c
@@ -0,0 +1,22 @@
+#include "rb_trees.h"
+
+/**
+ * array_to_rb_tree - Builds a Binary Search Tree from an array
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ *
+ * Return: Pointer to the root node of the created R-B tree, or NULL on failure
+ */
+rb_tree_t *array_to_rb_tree(int *array, size_t size)
+{
+	if (array == NULL || size == 0)
+		return (NULL);
+
+	rb_tree_t *root = NULL;
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		rb_tree_insert(&root, array[i]);
+
+	return (root);
+}
