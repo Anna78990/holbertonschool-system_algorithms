0b17babc54f57971a5cffa8365672d190a7c40b6
Anna78990 <takasuanna1990@gmail.com>
2023-06-09 04:35:24 +0000

    3

--- 0x00-red_black_tree/3-array_to_rb_tree.c
+++ 0x00-red_black_tree/3-array_to_rb_tree.c
@@ -9,11 +9,12 @@
  */
 rb_tree_t *array_to_rb_tree(int *array, size_t size)
 {
+	size_t i;
+
 	if (array == NULL || size == 0)
 		return (NULL);
 
 	rb_tree_t *root = NULL;
-	size_t i;
 
 	for (i = 0; i < size; i++)
 		rb_tree_insert(&root, array[i]);
