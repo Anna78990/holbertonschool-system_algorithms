b61d8d8bdd1e6f5ecb5e5044afc930975874faf6
Anna78990 <takasuanna1990@gmail.com>
2023-06-12 03:59:47 +0000

    5,6

--- 0x02-huffman_coding/huffman.h
+++ 0x02-huffman_coding/huffman.h
@@ -1,6 +1,8 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
+#include "heap.h"
+
 /**
  * struct symbol_s - Stores a char and its associated frequency
  *
@@ -12,5 +14,8 @@ typedef struct symbol_s
     char data;
     size_t freq;
 } symbol_t;
+
+symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 
 #endif
