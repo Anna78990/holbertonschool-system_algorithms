7ffb9552ae0e4cee118a14b9c0a7034bc78edb05
Anna78990 <takasuanna1990@gmail.com>
2023-06-22 09:58:12 +0000

    1-nary

--- 0x04-nary_trees/1-nary_tree_delete.c
+++ 0x04-nary_trees/1-nary_tree_delete.c
@@ -1,27 +1,38 @@
 #include "nary_trees.h"
 
 
+/**
+ * free_child - free tree children recursively
+ * @tree: tree to free
+ * @root: root of tree
+ */
 void free_child(nary_tree_t *tree, nary_tree_t *root)
 {
-    nary_tree_t *nxt, *chld;
+	nary_tree_t *nxt = NULL, *chld = NULL;
 
-    if (tree == NULL)
-    {
-        free(root);
-    	return;
-    }
-    if (tree->children)
-        chld = tree->children;
-    if (tree->next)
-        nxt = tree->next;
-    free(tree);
-    if (chld)
-        free_child(chld, root);
-    if (nxt)
-        free_child(nxt, root);
+	if (tree == NULL)
+	{
+		free(root->content);
+		free(root);
+		return;
+	}
+	if (tree->children)
+		chld = tree->children;
+	if (tree->next)
+		nxt = tree->next;
+	free(tree->content);
+	free(tree);
+	if (chld)
+		free_child(chld, root);
+	if (nxt)
+		free_child(nxt, root);
 }
 
+/**
+ * nary_tree_delete - free entire of given tree
+ * @tree: tree to free
+ */
 void nary_tree_delete(nary_tree_t *tree)
 {
-    free_child(tree, tree);
+	free_child(tree, tree);
 }
