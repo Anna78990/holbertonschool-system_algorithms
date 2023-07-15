4bb94a1c82eedd94a40eafe425c086fe19c8594c
Anna78990 <takasuanna1990@gmail.com>
2023-07-15 09:35:12 +0000

    huff_ex

--- 0x02-huffman_coding/huffman_extract_and_insert.c
+++ 0x02-huffman_coding/huffman_extract_and_insert.c
@@ -1,46 +1,47 @@
+#include <stdlib.h>
+#include "heap.h"
 #include "huffman.h"
 
 /**
- * huffman_extract_and_insert - extracts the two nodes of
- *				the priority queue and insert a new one
- * @priority_queue: pointer to the priority queue to extract from
- * Return: 1 on success or 0 on failure
+ * huffman_extract_and_insert - Extracts two nodes from the priority queue
+ *                              and inserts a new one
+ * @priority_queue: Pointer to the priority queue (min heap)
+ *
+ * Return: 1 on success, 0 on failure
  */
 int huffman_extract_and_insert(heap_t *priority_queue)
 {
-	if (priority_queue == NULL || priority_queue->size < 2)
-		return 0;
+	binary_tree_node_t *node1, *node2, *new_node;
+	symbol_t *symbol1, *symbol2, *new_symbol;
 
-	binary_tree_node_t *node1 = heap_extract(priority_queue);
-	binary_tree_node_t *node2 = heap_extract(priority_queue);
-
-	if (node1 == NULL || node2 == NULL)
+	if (!priority_queue)
+		return (0);
+	node1 = heap_extract(priority_queue);
+	node2 = heap_extract(priority_queue);
+	if (!node1 || node2)
 		return (0);
 
-	symbol_t *symbol1 = (symbol_t *)node1->data;
-	symbol_t *symbol2 = (symbol_t *)node2->data;
+	symbol1 = (symbol_t *)node1->data;
+	symbol2 = (symbol_t *)node2->data;
 
-	size_t new_freq = symbol1->freq + symbol2->freq;
-
-	symbol_t *new_symbol = symbol_create(-1, new_freq);
-	if (new_symbol == NULL)
+	new_symbol = symbol_create(-1, symbol1->freq + symbol2->freq);
+	if (!new_symbol)
 		return (0);
 
-	binary_tree_node_t *new_node = binary_tree_node(NULL, new_symbol);
-	if (new_node == NULL)
+	new_node = binary_tree_node(NULL, new_symbol);
+	if (!new_node)
 	{
-		symbol_free(new_symbol);
+		free(new_symbol);
 		return (0);
 	}
 
 	new_node->left = node1;
 	new_node->right = node2;
-	node1->parent = new_node;  // Corrected assignment order
-	node2->parent = new_node;  // Corrected assignment order
-
-	if (!heap_insert(priority_queue, new_node))
+	node1->parent = new_node;
+	node2->parent = new_node;
+	if (heap_insert(priority_queue, new_node) == NULL)
 	{
-		binary_tree_delete(new_node, symbol_free);
+		free(new_node);
 		return (0);
 	}
 
